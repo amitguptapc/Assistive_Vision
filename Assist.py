64d8d1674256ee5984f3f1180669e2c498d181e8
Amit <amitguptapc@gmail.com>
2020-06-01 13:39:32 +0530

    Added webcam input

--- Assist.py
+++ Assist.py
@@ -1,8 +1,30 @@
 from gtts import gTTS
 from playsound import playsound
 import Caption_it as ct
-caption = ct.caption_this_image('./sample/shore.jpg')
-print(caption)
-speech = gTTS(caption)
-speech.save('Audio/speech.wav')
-playsound('./Audio/speech.wav')
+import cv2
+import matplotlib.pyplot as plt
+plt.style.use('seaborn')
+
+
+n = 0
+plt.ion()
+while n<5:
+    cap = cv2.VideoCapture(0)
+    ret,img = cap.read()
+    if ret == False:
+        continue   
+    
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    plt.imshow(img)
+    plt.axis("off")
+    plt.imsave("./sample/image{}.jpg".format(n),img)
+    
+    caption = ct.predict_caption_greedy("sample/image{}.jpg".format(n))
+    plt.title(caption)
+    
+    print(caption)
+    speech = gTTS(caption)
+    speech.save('./audio/speech.wav')
+    playsound('./audio/speech.wav')
+    n += 1
+plt.show()
--- Caption_it.py
+++ Caption_it.py
@@ -1,60 +1,74 @@
-import numpy as np
-import keras
-import json
-import pickle
-from keras.applications.vgg16 import VGG16
-from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
-from keras.preprocessing import image
-from keras.models import Model, load_model
+from tensorflow.keras.models import Model, load_model
 from keras.preprocessing.sequence import pad_sequences
-from keras.utils import to_categorical
-from keras.layers import Input, Dense, Dropout, Embedding, LSTM
-from keras.layers.merge import add
+from tensorflow.keras.preprocessing import image
+import pickle
+import numpy as np
+import random
+import tensorflow as tf
+max_len = 74
 
-model = load_model('./models/model_19.h5')
-model._make_predict_function()
-
-model_temp = ResNet50(weights='imagenet',input_shape=(224,224,3))
-model_resnet = Model(model_temp.input, model_temp.layers[-2].output)
-model_resnet._make_predict_function()
-
-def preprocess_image(img):
-    img = image.load_img(img, target_size=(224,224))
-    img = image.img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img = preprocess_input(img)
-    return img
-
+# loading the models
+model = load_model('./features/model.h5')
+feature_extractor = load_model("./features/feature_extractor.h5")
+with open('./features/word2idx.pkl', 'rb') as f:
+    word2idx = pickle.load(f)
+with open('./features/idx2word.pkl', 'rb') as f:
+    idx2word = pickle.load(f)
+    
+# function to extract features from the input image    
 def encode_image(img):
-    img = preprocess_image(img)
-    feature_vector = model_resnet.predict(img)
-    feature_vector = feature_vector.reshape(1, feature_vector.shape[1])
-    return feature_vector
+    img = image.load_img(img,target_size=(299,299))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img,axis=0)
+    img = tf.keras.applications.inception_v3.preprocess_input(img)
+    feature_vector = feature_extractor.predict(img)
+    feature_vector = feature_vector.reshape((-1,))
+    return feature_vector    
 
-with open("./models/word_to_idx.pkl", 'rb') as w2i:
-    word_to_idx = pickle.load(w2i)
-
-with open("./models/idx_to_word.pkl", 'rb') as i2w:
-    idx_to_word = pickle.load(i2w)
-
-def predict_caption(photo):
-    in_text = "startseq"
-    max_len = 35
-    for i in range(max_len):
-        sequence = [word_to_idx[w] for w in in_text.split() if w in word_to_idx]
-        sequence = pad_sequences([sequence], maxlen=max_len, padding='post')
-        ypred =  model.predict([photo,sequence])
-        ypred = ypred.argmax()
-        word = idx_to_word[ypred]
-        in_text+= ' ' +word
-        if word =='endseq':
+# function to predict the caption using Beam Search
+def predict_caption_beamsearch(img, beam_index = 3):
+    start = [word2idx["<BEGIN>"]]
+    start_word = [[start, 0.0]]
+    while len(start_word[0][0]) < max_len:
+        temp = []
+        for s in start_word:
+            par_caps = pad_sequences([s[0]], maxlen=max_len, padding='post')
+            e = encode_image(img)
+            preds = model.predict([np.array([e]), np.array(par_caps)])
+            word_preds = np.argsort(preds[0])[-beam_index:]
+            # Getting the top <beam_index>(n) predictions and creating a 
+            # new list so as to put them via the model again
+            for w in word_preds:
+                next_cap, prob = s[0][:], s[1]
+                next_cap.append(w)
+                prob += preds[0][w]
+                temp.append([next_cap, prob])
+        start_word = temp
+        # Sorting according to the probabilities
+        start_word = sorted(start_word, reverse=False, key=lambda l: l[1])
+        # Getting the top words
+        start_word = start_word[-beam_index:]
+    start_word = start_word[-1][0]
+    intermediate_caption = [idx2word[i] for i in start_word]
+    final_caption = []
+    for i in intermediate_caption:
+        if i != '<END>':
+            final_caption.append(i)
+        else:
             break
-    final_caption =  in_text.split()
-    final_caption = final_caption[1:-1]
-    final_caption = ' '.join(final_caption)
+    final_caption = ' '.join(final_caption[1:])
     return final_caption
 
-def caption_this_image(image):
-    enc = encode_image(image)
-    caption = predict_caption(enc)
-    return caption
+# function to predict caption using Greedy Sampling
+def predict_caption_greedy(img):
+    start_word = ["<BEGIN>"]
+    while True:
+        par_caps = [word2idx[i] for i in start_word]
+        par_caps = pad_sequences([par_caps], maxlen=max_len, padding='post')
+        e = encode_image(img)
+        preds = model.predict([np.array([e]), np.array(par_caps)])
+        word_pred = idx2word[np.argmax(preds[0])]
+        start_word.append(word_pred)
+        if word_pred == "<END>" or len(start_word) > max_len:
+            break
+    return ' '.join(start_word[1:-1])
